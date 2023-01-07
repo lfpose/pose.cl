77a7eda9efa8c047a70fbf016c4777027f4d18b5
Lucas Pose <posemerani@gmail.com>
2023-01-07 18:23:32 -0300

    feat: fix github page sitename

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://lfpose.github.io',
   base: '/pose.cl',
 
   //output: 'server',
